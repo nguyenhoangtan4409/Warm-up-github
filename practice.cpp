cf24ec597a47a5e1c412201dce95ca3f249d5b7e
Hoang_Tan <nguyenhoangtan4409@gmail.com>
2021-07-31 12:26:26 +0700

    update reverse code

--- practice.cpp
+++ practice.cpp
@@ -15,7 +15,13 @@ void duplicateStr(char str[])
 
 void reverseStr(char str[])
 {
-   
+    char* str1; str1 = new char[strlen(str) + 1];
+    strcpy(str1, str);
+    int a = strlen(str);
+    for (int i = 0; i < a; i++) {
+        str[i] = str1[a - i - 1];
+
+    }
     cout << "reversed: " << str << endl;
 }
 
